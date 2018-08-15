cfcb6b57c085d4734524c16950f050bd2fd11fb5
luiztafarel <36997814+luiztafarel@users.noreply.github.com>
2018-08-14 23:26:35 -0300

    Inclusão  de seletores
    Inclusão  de seletores

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("teste");
+var
+    name = document.querySelector("#exampleInputName");
+    gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+    birth = document.querySelector("#exampleInputBirth");
+    country = document.querySelector("#exampleInputCountry");
+    email = document.querySelector("#exampleInputEmail");
+    password = document.querySelector("#exampleInputPassword");
+    photo = document.querySelector("#exampleInputFile");
+    adm = document.querySelector("#exampleInputAdmin");
