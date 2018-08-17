1dab4c6cf2b2257a379a581f233023e8c7855471
luiztafarel <36997814+luiztafarel@users.noreply.github.com>
2018-08-16 23:52:08 -0300

    Orientação a objetos com java script

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ var user = {};
 
 function addLine(dataUser) {
 
-    var tr = document.createElement("tr");
-    var td = document.createElement("td");
-
-    tr.innerHTML = `
+    document.getElementById("table-users").innerHTML = `
     <tr>
         <td> <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
@@ -19,8 +16,6 @@ function addLine(dataUser) {
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>
     </tr>`;
-
-    document.getElementById("table-users").appendChild(tr);
 
 }
 
@@ -45,6 +40,15 @@ document.getElementById("form-user-create").addEventListener("submit", function 
 
     });
 
-    addLine(user);
+    var objectUser = new User(user.name, 
+        user.gender, 
+        user.birth, 
+        user.country,
+        user.email, 
+        user.password, 
+        user.photo,
+        user.admin);
+
+    addLine(objectUser);
 
 });
