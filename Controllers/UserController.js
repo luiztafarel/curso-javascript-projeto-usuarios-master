dbf207cb5fb8c9ed3d60d9e3dbea35b541688ae2
luiztafarel <36997814+luiztafarel@users.noreply.github.com>
2018-08-18 22:26:40 -0300

    Adicinado o fileReader

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -15,9 +15,58 @@ class UserController {
 
             event.preventDefault();
 
-            let user = this.getValues();
+            let values = this.getValues();
 
-            this.addLine(user);
+            this.getPhoto().then(
+                (content) => {
+
+                    values.photo = content;
+
+                    this.addLine(values);
+
+                },
+                (e) => {
+                    console.error(e);
+
+                }
+            );
+
+        });
+
+    }
+
+    getPhoto() {
+
+        return new Promise((resolve, reject) => {
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+
+                if (item.name === 'photo') {
+                    return item;
+                }
+
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e) => {
+
+                reject(e);
+            }
+
+            if (file) {
+                fileReader.readAsDataURL(file)
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
 
         });
 
@@ -34,6 +83,10 @@ class UserController {
                 if (field.checked) {
                     user[field.name] = field.value;
                 }
+
+            } else if (field.name === "admin") {
+
+                user[field.name] = field.checked;
 
             } else {
 
@@ -59,19 +112,21 @@ class UserController {
 
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-            <tr>
-                <td> <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>`
-            ;
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
+            <td> <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? "Sim" : "Nâo"}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td> 
+        `;
+
+        this.tableEl.appendChild(tr);
 
     }
 
